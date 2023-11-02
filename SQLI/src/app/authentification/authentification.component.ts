124d89d3ec67254619f66feb83610828000246e8
salim <salimbekkari1998@gmail.com>
2023-11-02 10:36:28 +0100

    hhhhh

--- SQLI/src/app/authentification/authentification.component.ts
+++ SQLI/src/app/authentification/authentification.component.ts
@@ -1,22 +1,41 @@
-import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-authentification',
   templateUrl: './authentification.component.html',
   styleUrls: ['./authentification.component.css']
 })
-
+// Appelez la méthode pour masquer le Header
+  
 export class AuthentificationComponent{
   email: string = '';
   password: string = '';
+  isPasswordVisible: boolean = false;
+
   
-  constructor(private http: HttpClient) {}
+
+  constructor(private http: HttpClient, private router:Router) {
+  }
+
+  ngOnInit() {
+    // Appelez la méthode pour masquer le Header dans ngOnInit
+  }
+
+ 
+  togglePasswordVisibility() {
+    this.isPasswordVisible = !this.isPasswordVisible;
+  }
 
   onSubmit() {
+    this.router.navigate(['/events']);
+    /*
     const apiUrl = 'URL_DE_VOTRE_API';
     const headers = new HttpHeaders({ 'Content-Type': 'application/json' });
     const data={'email': this.email, 'password': this.password};
+
+    
     this.http.post(apiUrl, data, { headers }).subscribe(
       (response) => {
         console.log('Réponse du serveur :', response);
@@ -26,7 +45,7 @@ export class AuthentificationComponent{
         console.error('Erreur lors de la requête POST :', error);
       }
     );
-
+*/
   }
   
 }
