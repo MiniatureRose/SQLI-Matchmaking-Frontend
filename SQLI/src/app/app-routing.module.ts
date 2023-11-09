c6f976bcbfccce0457b6d8f1f35c267181c0b486
hnekt <hnekt@thor.enseirb-matmeca.fr>
2023-11-09 16:29:20 +0100

    adjusting auth page

--- SQLI/src/app/app-routing.module.ts
+++ SQLI/src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AuthentificationComponent } from './authentification/authentification.component';
 import { EventsComponent } from './events/events.component';
+import { SignupComponent } from './signup/signup.component';
 
 const routes: Routes = [
   {path: 'Authentification', component : AuthentificationComponent},
-  {path: 'Events', component : EventsComponent}
+  {path: 'Events', component : EventsComponent},
+  {path: 'signup', component : SignupComponent}
 
 ];
 
