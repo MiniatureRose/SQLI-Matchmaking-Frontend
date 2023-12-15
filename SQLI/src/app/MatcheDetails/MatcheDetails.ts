4fca9722e8f2650d77541f87b863e01ca5123069
Boumour Mouad <mouadboum2001@gmail.com>
2023-12-15 03:50:08 +0100

    Feat : registerMatch

--- SQLI/src/app/MatcheDetails/MatcheDetails.ts
+++ SQLI/src/app/MatcheDetails/MatcheDetails.ts
@@ -34,7 +34,7 @@ export class MatcheDetails {
   ngOnInit(){
     this.sharedservice.idMatch$.subscribe(value=>{
       this.idMatch=value;
-      // console.log(value);
+      console.log(value);
 
       ///////
       const apiUrl = 'http://localhost:8081/create/user';
@@ -58,7 +58,7 @@ export class MatcheDetails {
       {color : "#3062C8", imageUrl : "/assets/Player7.svg"}, 
       {color : "#3062C8", imageUrl : "/assets/Player7.svg"}, 
       {color : "#3062C8", imageUrl : "/assets/Player7.svg"}, 
-      {color : "#3062C8", imageUrl : "/assets/Player7.svg"}, 
+      // {color : "#3062C8", imageUrl : "/assets/Player7.svg"}, 
       {color : "#3062C8", imageUrl : "/assets/Player7.svg"}, 
       {color : "#3062C8", imageUrl : "/assets/Player2.svg"}]
     })
@@ -88,6 +88,29 @@ export class MatcheDetails {
 
   automaticChoice() {
 
+  }
+
+  registerMatch() {
+    const apiUrl = 'http://localhost:8081/create/matchuser';
+      const headers = new HttpHeaders({ 'Content-Type': 'application/json' });
+
+      // const userId = localStorage.getItem('userId'); // Retrieve user ID from storage
+
+      const data = {
+        "user_id": 3,
+        "match_id": this.idMatch
+      };
+      this.http.post(apiUrl, data, { headers }).subscribe(
+        (response) => {
+          console.log('Réponse du serveur :', response);
+        },
+        (error) => {
+          console.error('Erreur lors de la requête POST :', error);
+        }
+      );
+
+
+      // update users list 
   }
 
 }
